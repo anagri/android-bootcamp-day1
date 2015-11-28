45dd8b8a73707dcc894b68405f416091b6e68f7c
Amir <amir.nagri+git@gmail.com>
2015-11-28 15:27:14 +0530

    modified bmi app to use seekbar

--- app/src/main/java/co/creativev/helloworld/BMIActivity.java
+++ app/src/main/java/co/creativev/helloworld/BMIActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class BMIActivity extends AppCompatActivity {
@@ -13,16 +14,50 @@ public class BMIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
         final EditText inputName = (EditText) findViewById(R.id.inputName);
-        final EditText inputWeight = (EditText) findViewById(R.id.inputWeight);
-        final EditText inputHeight = (EditText) findViewById(R.id.inputHeight);
+        final SeekBar seekBarWeight = (SeekBar) findViewById(R.id.seekBarWeight);
+        final SeekBar seekBarHeight = (SeekBar) findViewById(R.id.seekBarHeight);
+        final TextView textViewWeight = (TextView) findViewById(R.id.textViewWeight);
+        final TextView textViewHeight = (TextView) findViewById(R.id.textViewHeight);
+        seekBarWeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewWeight.setText(String.format("%d kgs", seekBar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBarHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewHeight.setText(String.format("%d cms", seekBar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         final TextView output = (TextView) findViewById(R.id.textOutput);
         findViewById(R.id.buttonCalculate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String bmiStatus = null;
                 String name = inputName.getText().toString();
-                float weight = Float.parseFloat(inputWeight.getText().toString());
-                float height = Float.parseFloat(inputHeight.getText().toString());
+                float weight = seekBarWeight.getProgress();
+                float height = seekBarHeight.getProgress();
                 float bmi = weight / (height * height) * 10000;
                 if (bmi < 18.5)
                     bmiStatus = "UnderWeight";
